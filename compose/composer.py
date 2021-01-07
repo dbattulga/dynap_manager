841a61cc451c28b96709eb4e3e43c86a50cbfdd7
Davaadorj Battulga <davaadorj.battulga@u-hopper.com>
2021-01-07 14:49:35 +0100

    edit client

---
+++ compose/composer.py
@@ -0,0 +1,35 @@
+import yaml
+import json
+import requests
+import datetime as dt
+
+
+def run_conf():
+    with open('../conf/pipeline.yml') as f:
+        userconf = yaml.load(f)
+    userjobs = []
+    for userjob in userconf['jobs']:
+        userjobs.append(userjob['job_name'])
+        print("post: "+userjob['job_name'])
+
+        url = userjob['agent_address']
+
+        body = {'pipeline_name': userjob['pipeline_name'],
+            'job_name': userjob['job_name'],
+            'agent_address': userjob['agent_address'],
+            'source_broker': userjob['source_broker'],
+            'sink_broker': userjob['sink_broker'],
+            'source_topic': userjob['source_topic'],
+            'sink_topic': userjob['sink_topic'],
+            'entry_class': userjob['entry_class']
+            }
+        
+        files = [
+            ('jar', ('test.jar', open(userjob['job_path'], 'rb'), 'application/octet')),
+            ('data', ('data', json.dumps(body), 'application/json')),
+        ]
+        req = requests.post(url + ":5001/upload", files=files)
+        print(req)
+
+
+run_conf()
